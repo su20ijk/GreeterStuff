92db5c9297e446b6ebdbe4fa1492b67af1ae66b8
su20 <19liuq@elmbrookstudents.org>
2018-01-30 10:35:40 -0600

    Initial stage of the test class

---
+++ src/TestGreeter.java
@@ -0,0 +1,11 @@
+
+public class TestGreeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter test=new Greeter();
+		System.out.println("name:"+test.getName());
+		System.out.println("age:"+test.getAge());
+	}
+
+}
