9779dd13e31df006b1e27b90803a972bd9283410
su20 <19liuq@elmbrookstudents.org>
2018-01-30 10:38:54 -0600

    Modify Test Class
    add a array of greeters and display their states

--- src/TestGreeter.java
+++ src/TestGreeter.java
@@ -6,6 +6,13 @@ public class TestGreeter {
 		Greeter test=new Greeter();
 		System.out.println("name:"+test.getName());
 		System.out.println("age:"+test.getAge());
+		Greeter [] testArray=new Greeter[10];
+		for(int i=0; i<10; i++) {
+			testArray[i]=new Greeter();
+			System.out.println("name"+i+": "+testArray[i].getName());
+			System.out.println("age"+i+": "+testArray[i].getAge());
+		}
+		
 	}
 
 }
